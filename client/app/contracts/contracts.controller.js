8a4db4de04ac71cf380cbe2531d0000a9abf899a
crazyquark <sam.bitcoin@gmail.com>
2015-10-15 15:24:28 +0300

    add sockets around

--- client/app/contracts/contracts.controller.js
+++ client/app/contracts/contracts.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('webethApp')
-  .controller('ContractsCtrl', function ($scope) {
+  .controller('ContractsCtrl', function ($scope, socket) {
     $scope.message = 'Hello';
   });
--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/contracts', require('./api/contract'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
