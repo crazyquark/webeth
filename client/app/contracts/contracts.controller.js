5bb5c6b1c316663131004c3e16bd5815b2fc0cb4
crazyquark <sam.bitcoin@gmail.com>
2015-10-16 16:24:08 +0300

    create contract not working yet

--- client/app/contracts/contracts.controller.js
+++ client/app/contracts/contracts.controller.js
@@ -33,21 +33,5 @@ angular.module('webethApp')
             if (!$scope.compiledCode) {
                 return;
             }
-            
-            // let's assume that coinbase is our account
-            web3.eth.defaultAccount = web3.eth.coinbase;
-
-            web3.eth.contract(abi).new({ data: $scope.compiledCode }, function (err, contract) {
-                if (err) {
-                    console.error(err);
-                    return;
-                    // callback fires twice, we only want the second call when the contract is deployed
-                } else if (contract.address) {
-                    $scope.contract = contract;
-                    console.log('address: ' + $scope.contract.address);
-                    // document.getElementById('status').innerText = 'Mined!';
-                    // document.getElementById('call').style.visibility = 'visible';
-                }
-            });
         }
     });
--- server/services/ethereum/eth.service.js
+++ server/services/ethereum/eth.service.js
@@ -19,18 +19,35 @@ var EthService = {
 		fs.readFile(sourceFile, 'utf8', function (err, data) {
 			if (err) throw err;
 			debug('file: ' + data);
-				var compiled = web3.eth.compile.solidity(data);
-				debug(compiled);
-				
-				if (callback) {
-					callback(compiled);
-				}
-				
-				fs.unlink(sourceFile);
+			var compiled = web3.eth.compile.solidity(data);
+			debug(compiled);
+
+			if (callback) {
+				callback(compiled);
+			}
+
+			fs.unlink(sourceFile);
 		});
 
 		debug(contractName);
 		debug(sourceFile);
+	},
+	createContract: function (compiledCode) {
+		// let's assume that coinbase is our account
+		web3.eth.defaultAccount = web3.eth.coinbase;
+
+		web3.eth.contract(abi).new({ data: compiledCode }, function (err, contract) {
+			if (err) {
+				console.error(err);
+				return;
+				// callback fires twice, we only want the second call when the contract is deployed
+			} else if (contract.address) {
+				$scope.contract = contract;
+				console.log('address: ' + $scope.contract.address);
+				// document.getElementById('status').innerText = 'Mined!';
+				// document.getElementById('call').style.visibility = 'visible';
+			}
+		});
 	}
 };
 
