4679074aac56710fdbdcb094f31b689e91bee270
crazyquark <sam.bitcoin@gmail.com>
2015-10-16 15:49:13 +0300

    create account code

--- client/app/contracts/contracts.controller.js
+++ client/app/contracts/contracts.controller.js
@@ -28,4 +28,26 @@ angular.module('webethApp')
                 });
             }
         }
+
+        $scope.createContract = function () {
+            if (!$scope.compiledCode) {
+                return;
+            }
+            
+            // let's assume that coinbase is our account
+            web3.eth.defaultAccount = web3.eth.coinbase;
+
+            web3.eth.contract(abi).new({ data: $scope.compiledCode }, function (err, contract) {
+                if (err) {
+                    console.error(err);
+                    return;
+                    // callback fires twice, we only want the second call when the contract is deployed
+                } else if (contract.address) {
+                    $scope.contract = contract;
+                    console.log('address: ' + $scope.contract.address);
+                    // document.getElementById('status').innerText = 'Mined!';
+                    // document.getElementById('call').style.visibility = 'visible';
+                }
+            });
+        }
     });
