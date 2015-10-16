266f638f46d6c1ca67f79823d336dab6dbd38d88
crazyquark <sam.bitcoin@gmail.com>
2015-10-16 10:32:12 +0300

    prepare frontend for file upload

--- client/app/contracts/contracts.controller.js
+++ client/app/contracts/contracts.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('webethApp')
-  .controller('ContractsCtrl', function ($scope, socket) {
-    $scope.message = 'Hello';
+  .controller('ContractsCtrl', function ($scope, socket, Upload, $timeout) {
+    $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'upload/url',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }   
+    }
   });
