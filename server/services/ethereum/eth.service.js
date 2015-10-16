3a61793d824cd520ad22cdff75e17e11a2c788c0
crazyquark <sam.bitcoin@gmail.com>
2015-10-16 13:03:27 +0300

    test solc compiler from NodeJS

--- server/services/ethereum/eth.service.js
+++ server/services/ethereum/eth.service.js
@@ -1,7 +1,13 @@
 'use strict'
 
-var debug = require('debug')('Contracts')
+var debug = require('debug')('Contracts');
 var path = require('path');
+var fs = require('fs');
+var web3 = require('web3');
+
+function initWeb3() {
+	web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+}
 
 var EthService = {
 	processContractSource: function (fileData) {
@@ -9,10 +15,17 @@ var EthService = {
 		//filename: 'da96e132f4f5050f7a37658d48b9f4dd', path: 'uploads/da96e132f4f5050f7a37658d48b9f4dd', size: 31 }
 		var contractName = fileData.originalname;
 		var sourceFile = path.normalize(__dirname + '/../../../' + fileData.path);
+
+		var greeterSource = 'contract mortal { address owner; function mortal() { owner = msg.sender; } function kill() { if (msg.sender == owner) suicide(owner); } } contract greeter is mortal { string greeting; function greeter(string _greeting) public { greeting = _greeting; } function greet() constant returns (string) { return greeting; } }';
 		
+		var compiled = web3.eth.compile.solidity(greeterSource);
+
+		debug(compiled);
 		debug(contractName);
 		debug(sourceFile);
 	}
 };
+
+initWeb3();
 
 module.exports = EthService;
