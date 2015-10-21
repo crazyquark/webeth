10db26e793894794c71511f4b7cd8710045a35ea
crazyquark <sam.bitcoin@gmail.com>
2015-10-21 11:45:08 +0300

    remove more unused express routes

--- server/api/contract/index.js
+++ server/api/contract/index.js
@@ -8,11 +8,6 @@ var upload = multer({ dest: 'uploads/' })
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
 router.post('/', upload.single('file'), controller.upload);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
 
 module.exports = router;
