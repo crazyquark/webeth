adcb0b223ec6c979497173c435af7d3aae6cc400
andreicg <mudpedal@vertexarmy.org>
2015-10-22 11:28:23 +0300

    Implemented contract instance method calling

--- client/app/instances/instances.controller.js
+++ client/app/instances/instances.controller.js
@@ -1,40 +1,62 @@
 'use strict';
 
 angular.module('webethApp')
-  .controller('InstancesCtrl', function ($scope, $resource, $routeParams, socket) {
+  .controller('InstancesCtrl', function ($scope, $resource, $routeParams, socket, usSpinnerService) {
     $scope.message = 'Hello';
-    
+
     var contractId = $routeParams.id;
-    
-    socket.socket.on('post:list_instances', function(response) {
+
+    socket.socket.on('post:list_instances', function (response) {
       $scope.instances = response.instances;
       $scope.contractName = response.contractName;
-      
+
       try {
         var abiParsed = JSON.parse(response.abi);
         var methods = [];
         for (var key in abiParsed) {
           var abiElement = abiParsed[key];
-          
+
           if (abiElement.type === 'function' || abiElement.type === 'constructor') {
-            var isContructor = abiElement.type === 'constructor'; 
+            var isContructor = abiElement.type === 'constructor';
             var methodObject = {
-              name: (abiElement.name ? abiElement.name : response.contractName) + '()',
+              name: (abiElement.name ? abiElement.name : response.contractName),
               isConstructor: isContructor,
               isConstant: abiElement.constant
             };
             if (isContructor) {
-              methods.splice(0, 0, methodObject); 
+              methods.splice(0, 0, methodObject);
             } else {
               methods.push(methodObject);
             }
           }
         }
-        $scope.methods = methods; 
+        $scope.methods = methods;
       } catch (err) {
         $scope.methods = [];
-      } 
+      }
     });
-    
+
     socket.socket.emit('list_instances', contractId);
+
+    $scope.callMethod = function (instanceId, methodName) {
+      usSpinnerService.spin('contract-spin');
+      
+      socket.socket.on('post:call_method', function (response) {
+        console.log('done: ' + response);
+        usSpinnerService.stop('contract-spin');
+        if (response.isMethodConstant) {
+          swal('Great!', 'Your call returned "' + response.message + '"', 'success');
+        } else {
+          swal('Great!', 'Contract updated by transaction ' + response.txHash + ', your call returned ' + response.message, 'success');
+        }
+      });
+
+      socket.socket.on('error:call_method', function (err) {
+        usSpinnerService.stop('contract-spin');
+        console.log('error: ' + err);
+        swal('Oops!', 'Your call returned "' + err + '"', 'error');
+      });
+
+      socket.socket.emit('call_method', { instanceId: instanceId, methodName: methodName });
+    }
   });
--- server/api/contractInstance/contractInstance.socket.js
+++ server/api/contractInstance/contractInstance.socket.js
@@ -8,18 +8,74 @@ var ContractInstance = require('./contractInstance.model');
 var Contract = require('../contract/contract.model');
 var mongoose = require('mongoose');
 var Q = require('q');
+var web3 = require('web3');
 
-exports.register = function(socket) {
+exports.register = function (socket) {
   socket.on('list_instances', function (contractId) {
-    
-    var contractPromise   = Contract.findOneQ({_id: mongoose.Types.ObjectId(contractId)});
-    var instancesPromise  = ContractInstance.findQ({contractId: mongoose.Types.ObjectId(contractId)});
-    
-    Q.allSettled([contractPromise, instancesPromise]).then( function(results) {
+
+    var contractPromise = Contract.findOneQ({ _id: mongoose.Types.ObjectId(contractId) });
+    var instancesPromise = ContractInstance.findQ({ contractId: mongoose.Types.ObjectId(contractId) });
+
+    Q.allSettled([contractPromise, instancesPromise]).then(function (results) {
       var contract = results[0].value;
       var instances = results[1].value;
-      
-      socket.emit('post:list_instances', {instances: instances, abi: contract.abi, contractName: contract.name});
+
+      socket.emit('post:list_instances', { instances: instances, abi: contract.abi, contractName: contract.name });
     });
+  });
+
+  socket.on('call_method', function (params) {
+
+    var instanceId = params.instanceId;
+    var methodName = params.methodName;
+    
+    var errorHandle = function(err) {
+      socket.emit('error:call_method', err);
+    };
+    
+    ContractInstance.findOneQ({ _id: mongoose.Types.ObjectId(instanceId) }).then (function (instance) {
+      var contractPromise = Contract.findOneQ({ _id: mongoose.Types.ObjectId(instance.contractId) });
+      
+      contractPromise.then(function (contract) {
+        try {
+          var foundMethod = false;
+          var abiParsed = JSON.parse(contract.abi);
+          // search for the method inside the abi
+          for (var key in abiParsed) {
+            var abiElement = abiParsed[key];
+            if (abiElement.type === 'function' && abiElement.name === methodName) {
+              foundMethod = {
+                isMethodConstant: abiElement.constant,
+              };
+            }
+          }
+          
+          if (foundMethod) {
+            //do web3 operations
+            var contractObj = web3.eth.contract(abiParsed).at(instance.address);
+            
+            if (foundMethod.isMethodConstant) {
+              var response = contractObj[methodName]();
+              socket.emit('post:call_method', {
+                isMethodConstant: foundMethod.isMethodConstant,
+                message: response
+              });
+            } else {
+              var txHash = contractObj[methodName].sendTransaction({from:web3.eth.coinbase});
+              socket.emit('post:call_method', {
+                isMethodConstant: foundMethod.isMethodConstant,
+                txHash: txHash,
+              });
+            }
+          } else {
+            // method does not exist
+            Q.defer().reject(new Error('Method ' + methodName + ' does not exist!'));  
+          }
+        } catch (err) {
+          Q.defer().reject(new Error(err));
+        }
+      }, errorHandle);
+    }, errorHandle);
+
   });
 }
